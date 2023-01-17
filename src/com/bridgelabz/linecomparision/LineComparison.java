be61fc5daf559675a9826123da4f4def1e1ed0ce
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 15:33:54 +0530

    Added: Calculating Length

--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -15,6 +15,7 @@ public class LineComparison {
 		int y2 = scanner.nextInt();
 		double length;
 		
+		
 		length = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
 		System.out.println("Length Of line: "+length);
 
