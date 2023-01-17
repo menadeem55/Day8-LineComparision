d93bb8bf2d2ed045840ca108e961fabef86d91e0
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 15:35:29 +0530

    Added: Checking the equality

--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -3,21 +3,34 @@ package com.bridgelabz.linecomparision;
 import java.util.Scanner;
 
 public class LineComparison {
-	
+
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);// creating object
-		System.out.println("Enter Value X1 X2 Y1 Y2");  //Printing enter values
-		
-		//Input integers and calling the nextInt
+		System.out.println("Enter Value X1 X2 X3 X4"); // Printing enter values
 		int x1 = scanner.nextInt();
 		int x2 = scanner.nextInt();
+		int x3 = scanner.nextInt();
+		int x4 = scanner.nextInt();
+		System.out.println("Enter Value Y1 Y2 Y3 Y4");
 		int y1 = scanner.nextInt();
 		int y2 = scanner.nextInt();
-		double length;
-		
-		
-		length = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
-		System.out.println("Length Of line: "+length);
+		int y3 = scanner.nextInt();
+		int y4 = scanner.nextInt();
+		double length1;
+		double length2;
+
+		length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Length Of line1: " + length1);
+		length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+		System.out.println("Length Of Line2: " + length2);
+
+		// Checking condition equality
+		if (length1 == length2) {
+			System.out.println("Both Length Are Equal");
+		} else {
+			System.out.println("Not Equal");
+		}
 
 	}
 }
