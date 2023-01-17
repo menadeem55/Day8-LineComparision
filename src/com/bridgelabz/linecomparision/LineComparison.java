7813693b863aee4112e25af73afe749cac8bd7fb
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 15:31:24 +0530

    Added: welcome message

---
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM");
+		
+	}
+
+}
