e4c1ce549a928cc7a727debc77739753d4f8fb88
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 15:37:55 +0530

    Added: Comparison between lines

--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -7,7 +7,7 @@ public class LineComparison {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);// creating object
-		System.out.println("Enter Value X1 X2 X3 X4"); // Printing enter values
+		System.out.println("Enter Value X1 X2 X3 X4");
 		int x1 = scanner.nextInt();
 		int x2 = scanner.nextInt();
 		int x3 = scanner.nextInt();
@@ -20,16 +20,17 @@ public class LineComparison {
 		double length1;
 		double length2;
 
-		length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // Finding lengths
 		System.out.println("Length Of line1: " + length1);
 		length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
 		System.out.println("Length Of Line2: " + length2);
 
-		// Checking condition equality
 		if (length1 == length2) {
-			System.out.println("Both Length Are Equal");
+			System.out.println("Both Lines Are Equal");
+		} else if (length1 > length2) {
+			System.out.println("Length of First Line is Greater");
 		} else {
-			System.out.println("Not Equal");
+			System.out.println("Length of Second Line is Greater");
 		}
 
 	}
