9d3440c9c98ac0155d936f98f65d503f29589ee9
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-17 15:42:17 +0530

    Added: Comparing Line using Oops Concept

---
+++ src/com/bridgelabz/linecomparision/LineComaprisonMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linecomparision;
+
+public class LineComaprisonMain {
+	
+	public static void main(String[] args) {
+		
+		//Creating object
+		LineComparison linecomparison = new LineComparison();  
+		
+		//Calling method
+		linecomparison.getLenght();
+		linecomparison.compareLines();
+		
+	}
+}
+
--- src/com/bridgelabz/linecomparision/LineComparison.java
+++ src/com/bridgelabz/linecomparision/LineComparison.java
@@ -1,37 +1,47 @@
 package com.bridgelabz.linecomparision;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LineComparison {
 
-	public static void main(String[] args) {
+	// Initialize double length
+		static double length1, length2, length;
+		static int x1, x2, y1, y2;
 
-		Scanner scanner = new Scanner(System.in);// creating object
-		System.out.println("Enter Value X1 X2 X3 X4");
-		int x1 = scanner.nextInt();
-		int x2 = scanner.nextInt();
-		int x3 = scanner.nextInt();
-		int x4 = scanner.nextInt();
-		System.out.println("Enter Value Y1 Y2 Y3 Y4");
-		int y1 = scanner.nextInt();
-		int y2 = scanner.nextInt();
-		int y3 = scanner.nextInt();
-		int y4 = scanner.nextInt();
-		double length1;
-		double length2;
+		public void coordinates() {
 
-		length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // Finding lengths
-		System.out.println("Length Of line1: " + length1);
-		length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-		System.out.println("Length Of Line2: " + length2);
+			Scanner scanner = new Scanner(System.in);// creating object
+			System.out.println("Enter Value X1 X2 Y1 Y2");
+			
+			int x1 = scanner.nextInt();    //Input integers and calling the nextInt
+			int x2 = scanner.nextInt();
+			int y1 = scanner.nextInt();
+			int y2 = scanner.nextInt();
 
-		if (length1 == length2) {
-			System.out.println("Both Lines Are Equal");
-		} else if (length1 > length2) {
-			System.out.println("Length of First Line is Greater");
-		} else {
-			System.out.println("Length of Second Line is Greater");
+			length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));// finding lengths
 		}
 
+		public void getLenght() {
+			coordinates();
+			length1 = length;
+			System.out.println("Enter Value For second Line: ");
+			coordinates();
+			length2 = length;
+			System.out.println("Length of Line1: " + length1);
+			System.out.println("Length of Line2: " + length2);
+		}
+
+		public void compareLines() {
+			// value = lenOfLineFirst.compareTo(lenOfLineSecond);
+			if (length1 == length2)    //Checking length equality
+				System.out.println("Both lines are Equal.");
+			
+			//If condition checking the greater length
+			else if (length1 > length2)
+				System.out.println("Line1 is greater than Line2.");
+			else
+				System.out.println("Line1 is smaller than Line2.");
+			
+
+		}
 	}
-}
